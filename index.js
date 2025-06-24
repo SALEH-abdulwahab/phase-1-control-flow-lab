480184e0e527aa292444ac361f218e9dd4d1c5ed
SALEH-abdulwahab <saleh200228@gmail.com>
2025-06-24 12:36:32 +0300

    committed

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+  if (rideDistance <= 400) {
+    return "This one is on me!";
+  } else if (rideDistance < 2000) {
+    return "That will be twenty bucks.";
+  } else if (rideDistance < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+if (city === "NYC") {
+  return "Ok, sounds good.";
+} else  {
+return "No go.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip === "generous") {
+return "Thank you so much";
+}
+else if (tip === "not as generous") {
+return "Thank you.";
+} else {
+return "Bye.";
+}
 }
